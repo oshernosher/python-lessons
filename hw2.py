52728dabab7259983dcd940154814ee7b8efb619
Baruch <oshernosher@gmail.com>
2026-07-05 19:59:48 +0300

    First comment

---
+++ hw2.py
@@ -0,0 +1,51 @@
+# number = int(input("Enter a number: "))
+# print (number **2)
+
+# number = int(input("Enter the number: "))
+# print (number ** 5)
+# print (type)
+
+
+# a = int(input("Enter a number: "))
+# b = int(input("Enter another number: "))
+# c = int(input("Enter another number: "))
+# average = (a+b+c)/3
+# print (average)
+
+
+# minutes = int(input ("Enter the minutes: "))
+# print (minutes // 60)
+# print (minutes % 60)
+
+
+# price = int(input("Enter price: "))
+# discount = int(input("Enter discount: "))
+# discount_amount = price * discount / 100
+# final_price = price - discount_amount
+# print ("Final_price:", final_price)
+
+
+
+# number = int (input ("Enter the number"))
+# print(number % 10)
+
+# length = int(input("Enter length: "))
+# width = int(input("Enter width: "))
+# perimeter = 2 * (length + width)
+# print ("Perimeter:", perimeter)
+
+
+# number = int(input("Enter 4-digit number: "))
+#
+# a = number // 1000
+# b = (number // 100) % 10
+# c = (number // 10) % 10
+# d = number % 10
+# print(a)
+# print(b)
+# print(c)
+# print(d)
+
+
+
+
