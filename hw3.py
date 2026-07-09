2e99a42bdbc28a9d7d7a792d395c15145c418749
Baruch <oshernosher@gmail.com>
2026-07-09 21:38:18 +0300

    First comment

---
+++ hw3.py
@@ -0,0 +1,68 @@
+# number1 = int(input("Enter first number: "))
+# operation = input("Enter operation (+, -, *, /): ")
+# number2 = int(input("Enter second number: "))
+#
+# if operation == "+":
+#     print(number1 + number2)
+#
+# elif operation == "-":
+#     print(number1 - number2)
+#
+# elif operation == "*":
+#     print(number1 * number2)
+#
+# elif operation == "/":
+#     if number2 == 0:
+#         print("Cannot divide by zero")
+#     else:
+#         print(number1 / number2)
+# else:
+#     print("Unknown operation")
+
+
+
+# numbs = [12423, 55342, 4, 1, True, 11]
+# if len(numbs) > 1:
+#     last = numbs.pop(0)
+#     numbs.insert(0, last)
+#     print(numbs)
+
+
+
+# numbers = [1, 2, 3, 4, 5, 4]
+# result = []
+#
+# if len(numbers) == 0:
+#     result = [[], []]
+#
+# elif len(numbers) % 2 == 0:
+#     middle = len(numbers) // 2
+#
+#     result = [
+#         numbers[:middle],
+#         numbers[middle:]
+#     ]
+#
+# else:
+#     middle = len(numbers) // 2 + 1
+#
+#     result = [
+#         numbers[:middle],
+#         numbers[middle:]
+#     ]
+# print(result)
+
+
+
+
+
+# numbs = [5, 4, 3412, 435, True]
+# midlle_index = len(numbs) // 2
+# if len(numbs) % 2 != 0:
+#     midlle_index += 1
+# part1 = numbs[: midlle_index]
+# part2 = numbs[midlle_index:]
+# result = part1 , part2
+# print(result)
+
+
