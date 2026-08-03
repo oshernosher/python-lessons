1421f58366abd93ca01dd8f592e1e18b9e2bd567
Baruch <oshernosher@gmail.com>
2026-08-03 09:11:01 +0300

    First comment

--- hw10.py
+++ hw10.py
@@ -7,3 +7,47 @@ assert is_even(5) == False, 'Test2'
 assert is_even(0) == True, 'Test3'
 
 print('OK')
+
+
+def first_word(text):
+    """ Пошук першого слова """
+    text = text.replace(",", " ").replace(".", " ")
+
+    words = text.split()
+
+    return words[0]
+
+
+assert first_word("Hello world") == "Hello", 'Test1'
+assert first_word("greetings, friends") == "greetings", 'Test2'
+assert first_word("don't touch it") == "don't", 'Test3'
+assert first_word(".., and so on ...") == "and", 'Test4'
+assert first_word("hi") == "hi", 'Test5'
+assert first_word("Hello.World") == "Hello", 'Test6'
+
+print('OK')
+
+
+def pow(x):
+    return x ** 2
+
+
+def some_gen(begin, end, func):
+    """
+    begin: перший елемент послідовності
+    end: кількість елементів у послідовності
+    func: функція, яка формує значення для послідовності
+    """
+    for i in range(end):
+        yield begin
+        begin = func(begin)
+
+
+from inspect import isgenerator
+
+gen = some_gen(2, 4, pow)
+
+assert isgenerator(gen) == True, 'Test1'
+assert list(gen) == [2, 4, 16, 256], 'Test2'
+
+print('OK')
